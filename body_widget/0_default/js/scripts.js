628ec307d3fb92e8cd104b752214574aa41a32f9
kalinin <sergey.kalinin@simbirsoft.com>
2018-10-17 11:50:34 +0300

    use promise

--- body_widget/0_default/js/scripts.js
+++ body_widget/0_default/js/scripts.js
@@ -1,5 +1,3 @@
-console.log('widget start');
+console.log('-----------------------widget start');
 const tableEl = $('#table');
-console.log('tableEl', tableEl);
-console.log('tableEl html', tableEl.html());
 tableEl.on('click', () => { alert('click!'); });
